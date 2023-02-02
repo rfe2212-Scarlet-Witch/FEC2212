efa044afcacf7cfad801dd26f53ad2d28edaa226
Jared Funaro <jaredfunaro@gmail.com>
2023-02-01 19:59:31 -0500

    fixed sizes and amount, added add to cart functionality, fixed arrows, arrows and thumbnails sync up index, added checkmark to styles, styles now are in row of 4, started adding socials.

--- src/Components/OVProdInfo.jsx
+++ src/Components/OVProdInfo.jsx
@@ -1,5 +1,8 @@
-import React, {useState} from 'react';
-import OVStyle from './OVStyle.jsx'
+import React, {useState, useEffect} from 'react';
+import OVStyle from './OVStyle.jsx';
+import { BsTwitter } from 'react-icons/bs';
+import { GrInstagram } from 'react-icons/gr';
+import { FaFacebookF } from 'react-icons/fa';
 
 function OVProdInfo (props) {
   let oldPrice = '   '
@@ -11,6 +14,8 @@ function OVProdInfo (props) {
   const [currSize, setSize] = useState('');
   const [disabled, changeDisabled] = useState(true);
   const [cart, setCart] = useState([]);
+  const [heart, setHeart] = useState('false');
+  // const [amountTextState, setAmountTextState] = useState('-')
 
   let array = []
   let size = '';
@@ -18,7 +23,7 @@ function OVProdInfo (props) {
   // let disabled = false;
 
 
-  //handleClick for the add to bag button
+  //add to cart click handler
   const handleClick = () => {
     let newItem = {
       product: props.currProd.name,
@@ -30,43 +35,59 @@ function OVProdInfo (props) {
       thumbnail: props.displayedStyle.photos[0].thumbnail_url
     }
     let copyCart = cart;
-    copyCart.push(newItem);
-    setCart(copyCart);
+    if (!document.getElementById('select-size').value) {
+      document.getElementById('alert-message').removeAttribute('hidden');
     }
 
+    copyCart.push(newItem);
+    setCart(copyCart);
+    console.log('your cart, ', cart);
+    }
 
   //handleChange for the quantity selector
   const handleChangeAmount = (e) => {
     amount = Number(e.target.value) + 1;
     setAmount(amount);
   }
+
   //handleChange for the size selector
   const handleChange = (e) => {
-
     let i = 1;
     array = [];
-    while (i <= fakeObj[e.target.value].quantity) {
+    //create an array for the quantity selector to map through
+    if (fakeObj[e.target.value] !== undefined) {
+    while (i <= fakeObj[e.target.value].quantity && i<=15) {
       array.push(i);
       i++;
     }
+
+
+    document.getElementById('select-amount').removeAttribute('disabled');
     size = fakeObj[e.target.value].size;
     setSize(size);
     setAmountArray(array);
+    }
+    else {
+    // console.log('should be changing text');
+    // setAmountTextState('-');
+    }
+
+  }
+  // document.getElementById('select-amount').value === undefined
+  if (document.getElementById('select-size') === null) {
+    console.log('inside new');
   }
 
+  //change sale price
   if (props.displayedStyle.sale_price) {
     oldPrice = `$${props.displayedStyle.original_price}`
     currPrice = `$${props.displayedStyle.sale_price}  `
     textColor = 'red';
   }
 
-    //set the font of the size selector based on disabled state
-    let font = 'OUT OF STOCK'
-    // if (disabled) {
-    //   font = 'OUT OF STOCK';
-    // } else {
-    //   font = 'SELECT SIZE';
-    // }
+  let amountText =  '-';
+  //set the font of the size selector based on disabled state
+  let font = 'OUT OF STOCK'
 
   //set fakeObj to the displayed styles skus.
   let fakeObj = props.displayedStyle.skus || {fakeKey: 'fakeValue'};
@@ -75,14 +96,29 @@ function OVProdInfo (props) {
   //if the product has sizes availabe, remove disabled attribute and change the default text to SELECT SIZE
   if (Object.keys(fakeObj).length > 1) {
     document.getElementById("select-size").removeAttribute('disabled');
-    document.getElementById('select-amount').removeAttribute('disabled');
-    font = 'SELECT SIZE';
+    document.getElementById('default').setAttribute('disabled', true);
+    document.getElementById('cart').removeAttribute('hidden');
+    if (!document.getElementById('select-size').value) {
+      console.log('no size selected');
+    }
+    //if size is selected and no amount selected, default to 1 quantity
+    if (document.getElementById('select-size').value && !document.getElementById('select-amount').value) {
+      console.log('currently selected amount ', document.getElementById('select-amount').value);
+      document.getElementById('select-amount').value = 0;
+    }
+    font = 'SELECT SIZE'; //this line changes the text inside of the size selector
+  }
+
+  const handleHeart = () => {
+    // if (heart='♡') {
+    //   heart='♥';
+    // } else {
+    //   heart='♡';
+    // }
+    setHeart(!heart);
   }
 
 
-
-  // let salePrice = ${props.displayedStyle.sale_price} || null;
-  // let price = props.
   return (
     <div className='prodInfo'>
       <strong id='category' >
@@ -118,6 +154,8 @@ function OVProdInfo (props) {
         })}
       </div>
 
+      <div id='alert-message' hidden>Please select a size</div>
+
       <div id="select-size-container">
 
         <select onChange={handleChange} id="select-size" disabled>
@@ -128,19 +166,33 @@ function OVProdInfo (props) {
         </select>
 
         <select onChange={handleChangeAmount} id="select-amount" disabled>
-          <option value=''>SELECT AMOUNT</option>
+          <option id='default' value=''>-</option>
           {currAmountArray.map((value, key) => {
             return <option key={key} value={key}>{value}</option>
           })}
         </select>
+      </div>
 
+
+
+      <div id="cart-heart">
+        <button id='cart' onClick={handleClick} hidden>ADD TO CART</button>
+        {/* <button id='heart' onClick={handleHeart}>{!heart ? '♥' : '♡'}</button> */}
+      </div>
+
+      <div id="social">
+          <div id="twitter">
+          <BsTwitter size={25}/>
+          </div>
+          <div id="instagram">
+            <GrInstagram size={25}/>
+          </div>
+          <div>
+            <FaFacebookF size={25}/>
+          </div>
 
       </div>
 
-      <div id="bag-heart">
-        <button onClick={handleClick}>ADD TO BAG</button>
-        <button>heart</button>
-      </div>
 
     </div>
   )
